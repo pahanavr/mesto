5e31525473ad3e2e142642e251155aceb5c16f05
павел киселев <kiselev.pavel.job@gmail.com>
2022-06-15 19:11:46 +0300

    hotfix: some bugs in popup class

--- src/scripts/components/PopupWithImage.js
+++ src/scripts/components/PopupWithImage.js
@@ -2,8 +2,8 @@ import Popup from "./Popup.js";
 
 export default class PopupWithImage extends Popup {
     open(name, link) {
-        this._imageView = document.querySelector(".popup__image");
-        this._imageCaption = document.querySelector(".popup__image-caption");
+        this._imageView = this._popup.querySelector(".popup__image");
+        this._imageCaption = this._popup.querySelector(".popup__image-caption");
 
         this._imageView.src = link;
         this._imageView.alt = name;
